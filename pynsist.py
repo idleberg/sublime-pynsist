7b863610b51b90f5bca48f21fbd22bca7712213c
Jan T. Sott <jan@idleberg.com>
2017-09-10 16:34:08 +0200

    improve PEP8 conformity

--- pynsist.py
+++ pynsist.py
@@ -1,13 +1,14 @@
 import sublime
 from sublime_plugin import EventListener
 
+
 class PynsisCommand(EventListener):
     def on_load(self, view):
         if (view.file_name().endswith("/installer.cfg")):
-            if (view.settings().get('syntax').endswith("Pynsist.sublime-syntax")):
+            if (view.settings().get("syntax").endswith("Pynsist.sublime-syntax")):
                 return
 
-            auto_set_syntax = sublime.load_settings('Pynsist.sublime-settings').get("auto_set_syntax")
+            auto_set_syntax = sublime.load_settings("Pynsist.sublime-settings").get("auto_set_syntax")
 
             if auto_set_syntax is True:
                 view.set_syntax_file("Pynsist.sublime-syntax")
